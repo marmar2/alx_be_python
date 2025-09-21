867dd73c16830307440e37495172fcddff62df93
Mariam Maged Anwar <MarAnwar@MOBCORP.INTRT.COM>
2025-09-21 20:03:42 +0300

    fisrt commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1=10
+number2=5
+
+Addition = number1 + number2
+
+print("Addition of", number1, "and", number2, "is", Addition)
+
+Subtraction =  = number1 - number2
+
+print("Subtraction of", number1, "and", number2, "is", Subtraction)
+
+Multiplication  = number1 * number2
+
+print("Multiplication  of", number1, "and", number2, "is", Multiplication )
