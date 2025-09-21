86f64a3a140d978b7b785ca2f38b1993c59dcd8d
marmar2 <mariam3maged@gmail.com>
2025-09-21 20:07:00 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,11 +1,11 @@
-number1=10
-number2=5
+number1 = 10
+number2 = 5
 
 Addition = number1 + number2
 
 print("Addition of", number1, "and", number2, "is", Addition)
 
-Subtraction =  = number1 - number2
+Subtraction  = number1 - number2
 
 print("Subtraction of", number1, "and", number2, "is", Subtraction)
 
