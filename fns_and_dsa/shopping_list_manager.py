012e7c52c3c7b18c16c15257c1bae8526bc1d295
Mariam <mariam3maged@gmail.com>
2025-10-03 16:49:11 +0300

    shoppin2

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -20,7 +20,7 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = int(input("Enter your choice: "))
 
         if choice == '1':
             add(shopping_list, input("Add item: "))
