b999ff6e5fa5295a231e0c71ab54cf931368c4db
Mariam <mariam3maged@gmail.com>
2025-10-03 17:09:15 +0300

    operations4

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     if operation=="add":
       return num1+num2
     elif operation=="subtract":
@@ -7,7 +7,7 @@ def perform_operation(num1,num2,operation):
       return num1*num2
     elif operation=="divide":
       if num2==0:
-         return "error"
+        return "error"
       else: 
-         return num1/num2         
+        return num1/num2         
    
