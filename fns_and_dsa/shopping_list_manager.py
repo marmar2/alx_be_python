2e21fd58052ba16600fc500f9457ce2e44146d40
Mariam <mariam3maged@gmail.com>
2025-10-03 16:47:42 +0300

    shoppin1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,1 +1,41 @@
- 
+def add(list, x):
+
+  list.append(x)
+  
+def remove(list, x):
+
+  list.remove(x) 
+
+def view(list):
+   print(list)  
+  
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add(shopping_list, input("Add item: "))
+            pass
+        elif choice == '2':
+            remove(shopping_list, input("remove item: "))
+            pass
+        elif choice == '3':
+            view(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()  
