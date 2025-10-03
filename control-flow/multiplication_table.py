c910acf11e5750645f1cd7e663fe70240a91b2ab
Mariam <mariam3maged@gmail.com>
2025-10-03 14:03:28 +0300

    multi4

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
-number= float(input("Enter a number to see its multiplication table:"))
+number= int(input("Enter a number to see its multiplication table:"))
 
 for i in range(1,11):
    z=number*i
-   print(number,"*",i,"=",z)
+   print(f"{number} * {i} = {z}")
