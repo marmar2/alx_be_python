389214e7501d1e10c07b0372d0fe0885099c8516
Mariam <mariam3maged@gmail.com>
2025-10-03 16:57:34 +0300

    shoppin7

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -23,10 +23,10 @@ def main():
         choice = int(input("Enter your choice: "))
 
         if choice == 1:
-            add(shopping_list, int(input("Add item: ")))
+            add(shopping_list, int(input("Enter the item to add: ")))
             pass
         elif choice == 2:
-            remove(shopping_list, int(input("remove item: ")))
+            remove(shopping_list, int(input("Enter the item to remove: ")))
             pass
         elif choice == 3:
             view(shopping_list)
