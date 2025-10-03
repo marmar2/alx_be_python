6309113c5c909faceec5751da5a702d0b8045584
Mariam <mariam3maged@gmail.com>
2025-10-03 15:07:10 +0300

    daily4

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,19 +7,28 @@ match priority:
 
     case "high":
        text1="'"+str(task)+"' is a high priority task"
+       if time_bound=="yes" and (priority=="high" or priority=="medium"):
+         text="Reminder: "
+         text2= "that requires immediate attention today!" 
+         print(text,text1,text2) 
+       elif time_bound=="no" and (priority=="high" or priority=="medium"):
+         text="Note: "
+         text2= ". Consider completing it when you have free time."
+         print(text,text1,text2) 
     case "medium":
        text1="'"+str(task)+"' is a medium priority task" 
+       if time_bound=="yes" and (priority=="high" or priority=="medium"):
+         text="Reminder: "
+         text2= "that requires immediate attention today!" 
+         print(text,text1,text2) 
+       elif time_bound=="no" and (priority=="high" or priority=="medium"):
+         text="Note: "
+         text2= ". Consider completing it when you have free time."
+         print(text,text1,text2) 
     case "low":
        print("Note: '"+str(task)+"' is a low priority task. Consider completing it when you have free time.")
  
 
-if time_bound=="yes" and (priority=="high" or priority=="medium"):
-    text="Reminder: "
-    text2= "that requires immediate attention today!" 
-    print(text,text1,text2) 
-elif time_bound=="no" and (priority=="high" or priority=="medium"):
-    text="Note: "
-    text2= ". Consider completing it when you have free time."
-    print(text,text1,text2) 
+
      
       
