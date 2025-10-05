6bd7b2598c09e2f6ab644ad42a87341616176967
Mariam <mariam3maged@gmail.com>
2025-10-05 22:12:26 +0300

    cal1

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,27 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class Testcal(unittest.TestCase):
+
+    def setUp(self):
+        # This creates a calculator instance before each test
+        self.calc = SimpleCalculator()
+    
+    def test_add(self):
+        result = self.calc.add(4,3)
+        self.assertEqual(result,7)
+    def test_Subtract(self):
+        result = self.calc.subtract(4,3)
+        self.assertEqual(result,1)
+    def test_multiply(self):
+        result = self.calc.multiply(4,3)
+        self.assertEqual(result,12)    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4,2),2)
+        self.assertIsNone(self.calc.divide(4,0))
+        
+
+
+if __name__ == "__main__":
+  unittest.main()        
