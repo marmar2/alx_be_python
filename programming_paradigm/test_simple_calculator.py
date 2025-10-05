971d352fad8dd8b81e9950d8c5ad839311a081ac
Mariam <mariam3maged@gmail.com>
2025-10-05 22:16:15 +0300

    cal3

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,14 +9,11 @@ class Testcal(unittest.TestCase):
         self.calc = SimpleCalculator()
     
     def test_addition(self):
-        result = self.calc.add(4,3)
-        self.assertEqual(result,7)
+        self.assertEqual(self.calc.add(4,3),7)
     def test_Subtract(self):
-        result = self.calc.subtract(4,3)
-        self.assertEqual(result,1)
+        self.assertEqual(self.calc.subtract(4,3),1)
     def test_multiply(self):
-        result = self.calc.multiply(4,3)
-        self.assertEqual(result,12)    
+        self.assertEqual(self.calc.multiply(4,3),12)    
     def test_divide(self):
         self.assertEqual(self.calc.divide(4,2),2)
         self.assertIsNone(self.calc.divide(4,0))
