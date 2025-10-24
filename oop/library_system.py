1a6265adeadf2b04810745cda3bf0e460f47c667
Mariam <mariam3maged@gmail.com>
2025-10-24 15:53:47 +0300

    OOP library_system4

--- oop/library_system.py
+++ oop/library_system.py
@@ -22,7 +22,7 @@ class PrintBook(Book):
 class Library:
     def __init__(self):
         self.books = []
-        
+       
     def add_book(self, book):        
         self.books.append(book)
                        
